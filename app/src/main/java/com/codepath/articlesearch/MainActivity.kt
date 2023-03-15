cd446ee38e63b6346351ea8d9cfc9aa7e187ef12
tanveerm176 <tanveerm176@gmail.com>
2023-03-14 21:51:40 -0400

    setup main screen

--- app/src/main/java/com/codepath/articlesearch/MainActivity.kt
+++ app/src/main/java/com/codepath/articlesearch/MainActivity.kt
@@ -20,11 +20,12 @@ fun createJson() = Json {
 }
 
 private const val TAG = "MainActivity/"
-private const val SEARCH_API_KEY = BuildConfig.API_KEY
-private const val ARTICLE_SEARCH_URL =
+private val SEARCH_API_KEY = BuildConfig.API_KEY
+private val ARTICLE_SEARCH_URL =
     "https://api.nytimes.com/svc/search/v2/articlesearch.json?api-key=${SEARCH_API_KEY}"
 
 class MainActivity : AppCompatActivity() {
+    private val articles = mutableListOf<Article>()
     private lateinit var articlesRecyclerView: RecyclerView
     private lateinit var binding: ActivityMainBinding
 
@@ -37,6 +38,12 @@ class MainActivity : AppCompatActivity() {
 
         articlesRecyclerView = findViewById(R.id.articles)
         // TODO: Set up ArticleAdapter with articles
+
+
+        val articleAdapter = ArticleAdapter(this, articles)
+        articlesRecyclerView.adapter = articleAdapter
+
+
 
         articlesRecyclerView.layoutManager = LinearLayoutManager(this).also {
             val dividerItemDecoration = DividerItemDecoration(this, it.orientation)
@@ -58,10 +65,40 @@ class MainActivity : AppCompatActivity() {
                 Log.i(TAG, "Successfully fetched articles: $json")
                 try {
                     // TODO: Create the parsedJSON
+/*                    How does this work?
+
+                    The Serialization library helps us to take the information and convert it into Kotlin objects we can work with.
+                    Decoding is the part where serialization occurs and converts the JSON data into models, based on the objects we created in Step 1.*/
+                    val parsedJson = createJson().decodeFromString(
+                        SearchNewsResponse.serializer(),
+                        json.jsonObject.toString()
+                    )
 
                     // TODO: Do something with the returned json (contains article information)
+/*
+                    How does this work?
+
+                    Since the response object was the second outermost layer, we need to dig into our model using dot notation to get the articles:
+                    The response will have an array of docs, which will be our list of articles.
+                    Going through the docs array, we'll take each article and add it to our articles mutable list.
+*/
+
+                    parsedJson.response?.docs?.let { list ->
+                        articles.addAll(list)
+                    }
 
                     // TODO: Save the articles and reload the screen
+/*                    How does this work?
+
+                    You've probably noticed it in previous assignments and labs, but notifyDataSetChanged() is a very useful method built into the RecyclerView Adapter class.
+                    When called, notifyDataSetChanged() makes the RecyclerView reload and update the data it's displaying.
+                    You'll most likely want to call it any time you modify the data for a RecyclerView. */
+                    parsedJson.response?.docs?.let { list ->
+                        articles.addAll(list)
+
+                        // Reload the screen
+                        articleAdapter.notifyDataSetChanged()
+                    }
 
                 } catch (e: JSONException) {
                     Log.e(TAG, "Exception: $e")
